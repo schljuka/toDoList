5f5dd398f4874913f147c1813a60d8a565112d2c
schljuka <142089243+schljuka@users.noreply.github.com>
2024-03-24 08:06:32 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -3,14 +3,39 @@ document.addEventListener("DOMContentLoaded", function () {
     const lista = document.querySelector('#lista');
     const todoColumn = document.querySelector('#todotask');
 
+    function deleteTask(task) {
+        task.remove();
+    }
+
+
+
     document.querySelector('#dodaj').addEventListener("click", function () {
         const taskText = taskInput.value;
         const selectedValue = lista.value;
+
 
         if (taskText.trim() !== "") {
             const noviTask = document.createElement("div");
             noviTask.textContent = taskText;
             noviTask.className = "task " + selectedValue;
+
+            //označavanje zadatka kao završenog
+            const checkbox = document.createElement("input");
+            checkbox.type = "checkbox";
+            checkbox.addEventListener('change', function () {
+                toggleCompleted(noviTask);
+            });
+
+            //brisanje zadatka
+            const deleteButton = document.createElement("button");
+            deleteButton.textContent = "x";
+            deleteButton.classList.add("delete-btn");
+            deleteButton.addEventListener("click", function () {
+                deleteTask(noviTask);
+            });
+
+            noviTask.appendChild(checkbox);
+            noviTask.appendChild(deleteButton);
 
             todoColumn.appendChild(noviTask);
 
@@ -19,4 +44,5 @@ document.addEventListener("DOMContentLoaded", function () {
             alert("Please enter a task.");
         }
     });
+
 });
